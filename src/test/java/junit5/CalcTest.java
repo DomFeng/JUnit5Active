0fb2d4f0f5d15e213a3423094336f9e677bb7ce2
domfeng <domfeng@163.com>
2020-07-30 17:57:28 +0800

    JUnit5Actual

--- src/test/java/junit5/CalcTest.java
+++ src/test/java/junit5/CalcTest.java
@@ -1,6 +1,7 @@
 package junit5;
 
 import Junit5.Calc;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalcTest {
 
     @Test
+    @Tag("xxx")
     void div() {
         Calc calc = new Calc();
         assertEquals(calc.div(2,1),2);
---
+++ src/test/java/junit5/TestSuite.java
@@ -0,0 +1,14 @@
+package junit5;
+
+import Junit5.Calc;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.SuiteDisplayName;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SuiteDisplayName("JUnit Platform Suite Demo")
+@SelectClasses(DynamicTestsDemo.class)
+public class TestSuite {
+}
